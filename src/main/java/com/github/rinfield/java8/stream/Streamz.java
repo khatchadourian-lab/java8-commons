054883ab44df227654cd0351f55abe2de1c46a5b
rinfield <rinfield@gmail.com>
2015-01-08 23:01:47 +0900

    add factory methods

--- src/main/java/com/github/rinfield/java8/stream/Streamz.java
+++ src/main/java/com/github/rinfield/java8/stream/Streamz.java
@@ -1,9 +1,17 @@
 package com.github.rinfield.java8.stream;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Spliterator;
+import java.util.Spliterators;
 import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 import java.util.stream.BaseStream;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public interface Streamz<T> extends BaseStream<T, Streamz<T>>,
     StreamExtTerm<T>, StreamExtTermShort<T>, StreamExtInter<T>,
@@ -11,6 +19,33 @@ public interface Streamz<T> extends BaseStream<T, Streamz<T>>,
 
     static <T> Streamz<T> of(final Stream<T> stream) {
         return new StreamzImpl<>(stream);
+    }
+
+    static <T> Streamz<T> of(final Collection<T> collection) {
+        return of(collection.stream());
+    }
+
+    static <K, V> Streamz<Map.Entry<K, V>> of(final Map<K, V> map) {
+        return of(map.entrySet());
+    }
+
+    static <T> Streamz<T> of(final Enumeration<T> enumeration) {
+        return of(Collections.list(enumeration).stream());
+    }
+
+    static <T> Streamz<T> of(final Iterator<T> iterator) {
+        return of(StreamSupport.stream(
+            Spliterators.spliteratorUnknownSize(iterator, Spliterator.ORDERED),
+            false));
+    }
+
+    @SuppressWarnings("unchecked")
+    static <T> Streamz<T> of(final T... values) {
+        return of(Stream.of(values));
+    }
+
+    static <T> Streamz<T> of(final T value) {
+        return of(Stream.of(value));
     }
 
     static <T> Streamz<T> empty() {
@@ -25,12 +60,4 @@ public interface Streamz<T> extends BaseStream<T, Streamz<T>>,
         return of(Stream.iterate(seed, f));
     }
 
-    @SuppressWarnings("unchecked")
-    static <T> Streamz<T> of(final T... values) {
-        return of(Stream.of(values));
-    }
-
-    static <T> Streamz<T> of(final T value) {
-        return of(Stream.of(value));
-    }
 }
